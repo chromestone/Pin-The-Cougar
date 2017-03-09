33d3dbd9e1969e31becd2575f0b09d0eff0da014
bhuynh987 <bhuynh987@gmail.com>
2017-03-09 09:45:26 -0500

    Add files via upload

---
+++ RandomAssignment.java
@@ -0,0 +1,17 @@
+package Defalult;
+public class RandomAssignment extends Assassin
+{
+	private static String fName;
+	private static String lName;
+	private static int id;
+	
+	public RandomAssignment(String a, String b, int c)
+	{
+		fName = a;
+		lName = b;
+		id = c;
+	}
+	
+	
+	
+}
