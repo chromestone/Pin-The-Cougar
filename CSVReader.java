e9724db5446adf83fa441f6e36b3807951bfd782
Derek Zhang <derekzhang@Dereks-MacBook-Pro.local>
2017-03-16 10:04:04 -0400

    Reader
    hope it works. need to do testing though

---
+++ CSVReader.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Derek Zhang
+ *
+ */
+public final class CSVReader {
+
+	private CSVReader() {}
+
+	public static ArrayList<Assassin> read(String filePath) throws FileNotFoundException {
+				
+		File file = new File(filePath);
+		
+		Scanner scanner = new Scanner(file);
+		
+		ArrayList<Assassin> aList = new ArrayList<Assassin>();
+		
+		if (!scanner.hasNextLine()) {
+			
+			scanner.close();
+			return aList;
+		}
+		
+		//ignore first line
+		scanner.nextLine();
+		
+		while (scanner.hasNextLine()) {
+			
+			String aString = scanner.nextLine();
+			String[] values = aString.split(",");
+			aList.add(new Assassin(Integer.valueOf(values[3]).intValue(), values[1], values[2], values[5], null));
+		}
+		
+		scanner.close();
+		
+		return aList;
+	}
+}
